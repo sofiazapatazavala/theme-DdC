079c7403a28f3e9d9e3f9a4802a94a3d4c4eb896
Sofía Zapata Zavala <sofiazapatazavala@gmail.com>
2016-12-08 17:18:40 -0300

    Arreglos a la portada

---
+++ js/jquery-actions.js
@@ -0,0 +1,16 @@
+$(function() {
+    // Transforma en botón los elementos para editar entradas
+    $(".post-edit-link").addClass("btn btn-block btn-secondary");
+    $(".type-ddc_documentos .content a:first-child").addClass("btn btn-block btn-primary");
+    // Si se usa opción de imagen de equipo, activar
+    /*
+    $("img.img-equipo").mouseenter(function() {
+        $(this).css('display','none'); $(this).next().css('display','block');
+    } );
+    $("img.img-equipo-hover").mouseleave(function() {
+        $(this).css('display','none'); $(this).prev().css('display','block');
+    } );
+    $(".equipo-content p").addClass("small");
+    */
+
+} );
--- page-portada.php
+++ page-portada.php
@@ -39,10 +39,48 @@ get_template_part('header', 'fullscreen');
     <h1 class="my-2">Documentos</h1>
   </div>
 </div>
+<div class="row the-docs">
+  <?php
+    $my_query = new WP_Query( 'post_type=ddc_documentos&posts_per_page=3' );
+    while ( $my_query->have_posts() ) : $my_query->the_post();
+    $do_not_duplicate[] = $post->ID;
+  ?>
+
+  <div class="col-sm-4">
+    <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+      <div class="card">
+        <div class="card-block">
+          <header class="entry-header">
+            <div align="center">
+              <?php echo get_the_post_thumbnail( $page->ID, 'portada', array( 'class' => 'img-responsive' ) ); ?>
+            </div>
+            <h6 class="text-center categoria-portada"><?php the_category('&bull;'); ?></h6>
+            <?php the_title( sprintf( '<h3 class="text-center"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h3>' ) ?>
+          </header>
+          <div class="content">
+            <?php the_content(); ?>
+          </div>
+          <footer class="entry-footer">
+          <?php edit_post_link( __( 'Editar', 'RD-DdC' ) ); ?>
+          </footer><!-- .entry-footer -->
+        </div>
+      </div>
+    </article>
+  </div><!-- #post-## -->
+  <?php endwhile; ?>
+</div>
+<?php
+// Agenda: Próximamente (implementación con plugin)
+
+/*
+
 <div class="row">
   <div class="col-md-12">
     <h1 class="my-2">Agenda</h1>
   </div>
 </div>
+
+*/
+?>
 
 <?php get_footer(); ?>
