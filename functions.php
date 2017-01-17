47e5b05f1b23d1a65ccf9f8b42e3a1373123dee1
Sofía Zapata Zavala <sofiazapatazavala@gmail.com>
2017-01-16 21:51:22 -0300

    Solucionado bug de títulos

--- functions.php
+++ functions.php
@@ -22,16 +22,11 @@ add_image_size( 'portada', 390, 260, true  );
 add_image_size( 'equipo', 360, 360, true  );
 }
 
-// Título para portada
-
-add_filter( 'wp_title', 'baw_hack_wp_title_for_home' );
-function baw_hack_wp_title_for_home( $title )
-{
-  if( empty( $title ) && ( is_home() || is_front_page() ) ) {
-    return __( 'Portada' ) . ' | ' . get_bloginfo( 'name' );
-  }
-  return $title;
+// Títulos
+function theme_slug_setup() {
+   add_theme_support( 'title-tag' );
 }
+add_action( 'after_setup_theme', 'theme_slug_setup' );
 
 // Custom header para mostrar en la portada (en noticias se usará la imagen destacada).
 if ( function_exists( 'add_theme_support' ) ) {
